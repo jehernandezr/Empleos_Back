aa810dd9a3bca91227ac803eed9085d5214f0db2
David Dominguez <ld.dominguez@uniandes.edu.co>
2019-08-20 18:42:49 -0500

    Se agrega EstudiantePersistence

--- s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/persistence/EstudiantePersistence.java
+++ s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/persistence/EstudiantePersistence.java
@@ -1,14 +1,21 @@
 /*
+
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package co.edu.uniandes.csw.empleos.persistence;
 
+import co.edu.uniandes.csw.empleos.entities.EstudianteEntity;
+
 /**
  *
  * @author Estudiante
  */
 public class EstudiantePersistence {
+    
+    public EstudianteEntity create(EstudianteEntity e) {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
     
 }
