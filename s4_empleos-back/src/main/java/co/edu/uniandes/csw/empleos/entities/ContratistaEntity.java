d8acf7623ca69779949da80ac5b4c6016ee6e0ff
jeberdugo10 <je.berdugo10@uniandes.edu.co>
2019-08-22 00:13:49 -0500

    Entidad y persistencia de la clase contratista, con pruebas implementadas

---
+++ s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/entities/ContratistaEntity.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.empleos.entities;
+
+import javax.persistence.Entity;
+
+/**
+ *Clase que representa un contratista para poder ser guardado en la base de datos
+ * @author Estudiante
+ */
+@Entity
+public class ContratistaEntity extends BaseEntity {
+    
+    /**
+     * Variable que representa si el contratista es externo.
+     */
+    private boolean esExterno;
+    
+    /**
+     * Variable que representa el nombre del contratista.
+     */
+    private String nombre;
+    
+    /**
+     * Variable que representa el nombre del contratista.
+     */
+    private String email;
+    
+    /**
+     * Variable que representa la contraseña del contratista.
+     */
+    private String contrasena;
+    
+     /**
+     * Variable que representa la ruta de la imagen del contratista.
+     */
+    private String rutaImagen;
+
+   
+
+    
+
+   
+    /**
+     * Constructor del contratista
+     */
+    public ContratistaEntity(){
+        
+    }
+    
+    /**
+     * Constructor del contratista
+     * @param esExterno si es unidad externa o interna
+     * @param nombre del contratista
+     * @param email del contratisa
+     * @param contrasena del contratista
+     * @param rutaImagen
+     */
+   public ContratistaEntity(boolean esExterno, String nombre, String email, String contrasena, String rutaImagen){
+       this.esExterno=esExterno;
+       this.nombre=nombre;
+       this.email=email;
+       this.contrasena=contrasena;
+       this.rutaImagen=rutaImagen;
+   }
+   
+   /**
+     * Devuelve el nombre del contratista.
+     * @return true si el contratista es externo y false si es interno.
+     */
+   public String getNombre(){
+       return nombre;
+   }
+   
+   /**
+     * Actualiza el nombre del contratista.
+     * @param nombre del contratista.
+     */
+   public void setNombre(String nombre){
+       this.nombre=nombre;
+   }
+  
+    /**
+     * Devuelve si el contratista es externo.
+     * @return true si el contratista es externo y false si es interno.
+     */
+   public boolean getEsExterno(){
+       return esExterno;
+   }
+   
+    /**
+     * Actualiza el estado del contratista.
+     * @param esExterno true si el contratista es externo y false si es interno.
+     */
+   public void setEsExterno(boolean esExterno){
+       this.esExterno=esExterno;
+   }
+   
+    /**
+     * Obetener el email del contratista.
+     * @return email del contratista
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Actualiza el email del contratista.
+     * @param email a modificar
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    /**
+     * Obetener la contraseña del contratista.
+     * @return contraseña del contratista
+     */    
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * Actualiza la contraseña del contratista.
+     * @param contrasena a modificar
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
+     /**
+     * Obetener la ruta de la imagen del contratista.
+     * @return ruta de la imagen del contratista
+     */ 
+     public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+     /**
+     * Actualiza la ruta de la imagen del contratista.
+     * @param rutaImagen a modificar
+     */
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+            
+    
+}
