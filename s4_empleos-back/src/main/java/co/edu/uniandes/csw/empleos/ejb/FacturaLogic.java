483de5d296e602575d99437c4cada58071228dea
Nicolas Munar <n.munar@uniandes.edu.co>
2019-09-03 18:32:25 -0500

    Factura

--- s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/ejb/FacturaLogic.java
+++ s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/ejb/FacturaLogic.java
@@ -5,10 +5,25 @@
  */
 package co.edu.uniandes.csw.empleos.ejb;
 
+import co.edu.uniandes.csw.empleos.entities.FacturaEntity;
+import co.edu.uniandes.csw.empleos.persistence.FacturaPersistence;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
 /**
  *
- * @author Estudiante
+ * @author Nicolas Munar
  */
+@Stateless
 public class FacturaLogic {
+    
+    @Inject
+    private FacturaPersistence persistence;
+    
+    
+    public FacturaEntity createFactura(FacturaEntity factura){
+        factura = persistence.create(factura);
+        return factura;
+    }
     
 }
