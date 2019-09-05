0036c756f0271afa4942fd4591077e139961f7e2
jeberdugo10 <je.berdugo10@uniandes.edu.co>
2019-09-05 17:40:12 -0500

    Implementacion logica

--- s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/ejb/ContratistaLogic.java
+++ s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/ejb/ContratistaLogic.java
@@ -6,6 +6,7 @@
 package co.edu.uniandes.csw.empleos.ejb;
 
 import co.edu.uniandes.csw.empleos.entities.ContratistaEntity;
+import co.edu.uniandes.csw.empleos.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.empleos.persistence.ContratistaPersistence;
 import java.util.List;
 import java.util.logging.Level;
@@ -53,49 +54,16 @@ public class ContratistaLogic {
      *
      * @param contratistaId Identificador de la instancia a consultar
      * @return Instancia de ContratistaEntity con los datos del Author consultado.
+     * @throws co.edu.uniandes.csw.empleos.exceptions.BusinessLogicException 
      */
-    public ContratistaEntity getContratista(Long contratistaId) throws BussinessLogicException {
+    public ContratistaEntity getContratista(Long contratistaId) throws BusinessLogicException {
         LOGGER.log(Level.INFO, "Inicia proceso de consultar el contratista con id = {0}", contratistaId);
         ContratistaEntity contratistaEntity = persistence.find(contratistaId);
         if (contratistaEntity == null) {
-            
+            throw new BusinessLogicException("No existe el contratista con el id "+contratistaId);
         }
         LOGGER.log(Level.INFO, "Termina proceso de consultar el autor con id = {0}", contratistaId);
         return contratistaEntity;
     }
 
-    /**
-     * Actualiza la información de una instancia de Author.
-     *
-     * @param authorsId Identificador de la instancia a actualizar
-     * @param authorEntity Instancia de AuthorEntity con los nuevos datos.
-     * @return Instancia de AuthorEntity con los datos actualizados.
-     */
-    public AuthorEntity updateAuthor(Long authorsId, AuthorEntity authorEntity) {
-        LOGGER.log(Level.INFO, "Inicia proceso de actualizar el autor con id = {0}", authorsId);
-        AuthorEntity newAuthorEntity = persistence.update(authorEntity);
-        LOGGER.log(Level.INFO, "Termina proceso de actualizar el autor con id = {0}", authorsId);
-        return newAuthorEntity;
-    }
-
-    /**
-     * Elimina una instancia de Author de la base de datos.
-     *
-     * @param authorsId Identificador de la instancia a eliminar.
-     * @throws BusinessLogicException si el autor tiene libros asociados.
-     */
-    public void deleteAuthor(Long authorsId) throws BusinessLogicException {
-        LOGGER.log(Level.INFO, "Inicia proceso de borrar el autor con id = {0}", authorsId);
-        List<BookEntity> books = getAuthor(authorsId).getBooks();
-        if (books != null && !books.isEmpty()) {
-            throw new BusinessLogicException("No se puede borrar el autor con id = " + authorsId + " porque tiene books asociados");
-        }
-        List<PrizeEntity> prizes = getAuthor(authorsId).getPrizes();
-        if (prizes != null && !prizes.isEmpty()) {
-            throw new BusinessLogicException("No se puede borrar el autor con id = " + authorsId + " porque tiene premios asociados");
-        }
-        persistence.delete(authorsId);
-        LOGGER.log(Level.INFO, "Termina proceso de borrar el autor con id = {0}", authorsId);
-    }
-    
 }
