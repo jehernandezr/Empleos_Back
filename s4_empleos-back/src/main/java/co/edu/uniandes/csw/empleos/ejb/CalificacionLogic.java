1a49c0455ecb5ccfd844ef7db036c6327142d1e6
Nicolas Munar <n.munar@uniandes.edu.co>
2019-09-09 01:04:35 -0500

    Cambios Logic

---
+++ s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/ejb/CalificacionLogic.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.empleos.ejb;
+
+import co.edu.uniandes.csw.empleos.entities.CalificacionEntity;
+import co.edu.uniandes.csw.empleos.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.empleos.persistence.CalificacionPersistence;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Nicolás Munar
+ */
+@Stateless
+public class CalificacionLogic {
+    
+     @Inject
+    private CalificacionPersistence persistence;
+    
+    
+    public CalificacionEntity createCalificacion(CalificacionEntity calificacion) throws BusinessLogicException{
+        if(calificacion.getNota()<0){
+            throw new BusinessLogicException("La nota del estudiante no puede tener valores negativos");
+        }
+        if(calificacion.getComentario()==null){
+            throw new BusinessLogicException("El comentario de un estudainte no puede ser nulo");
+        }
+        
+        calificacion = persistence.create(calificacion);
+        return calificacion;
+    }
+    
+}
--- s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/ejb/FacturaLogic.java
+++ s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/ejb/FacturaLogic.java
@@ -6,6 +6,7 @@
 package co.edu.uniandes.csw.empleos.ejb;
 
 import co.edu.uniandes.csw.empleos.entities.FacturaEntity;
+import co.edu.uniandes.csw.empleos.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.empleos.persistence.FacturaPersistence;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -21,7 +22,18 @@ public class FacturaLogic {
     private FacturaPersistence persistence;
     
     
-    public FacturaEntity createFactura(FacturaEntity factura){
+    public FacturaEntity createFactura(FacturaEntity factura) throws BusinessLogicException{
+        
+        
+        if(factura.getValor()<0)
+        {
+            throw new BusinessLogicException ("El valor de la factura no puede tener valores negativos");
+        }
+        if(factura.getFecha()==null)
+        {
+            throw new BusinessLogicException("La fecha no puede ser una valor nulo");
+        }
+
         factura = persistence.create(factura);
         return factura;
     }
