dff30607264a446adb9847944924589cab566b75
Nicolas Munar <n.munar@uniandes.edu.co>
2019-09-12 11:29:52 -0500

    Arreglos clases Logic

--- s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/logic/CalificacionLogic.java
+++ s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/logic/CalificacionLogic.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.edu.uniandes.csw.empleos.ejb;
+package co.edu.uniandes.csw.empleos.logic;
 
 import co.edu.uniandes.csw.empleos.entities.CalificacionEntity;
 import co.edu.uniandes.csw.empleos.exceptions.BusinessLogicException;
