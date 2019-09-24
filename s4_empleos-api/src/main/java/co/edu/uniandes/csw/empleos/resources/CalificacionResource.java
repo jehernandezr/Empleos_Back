c6417b5f66743d29600d40433e2f7498168f8fb8
Nicolas Munar <n.munar@uniandes.edu.co>
2019-09-23 22:37:47 -0500

    Mejoras postman

--- s4_empleos-api/src/main/java/co/edu/uniandes/csw/empleos/resources/CalificacionResource.java
+++ s4_empleos-api/src/main/java/co/edu/uniandes/csw/empleos/resources/CalificacionResource.java
@@ -6,8 +6,11 @@
 package co.edu.uniandes.csw.empleos.resources;
 
 import co.edu.uniandes.csw.empleos.dtos.CalificacionDTO;
+import co.edu.uniandes.csw.empleos.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.empleos.logic.CalificacionLogic;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -23,9 +26,16 @@ import javax.ws.rs.Produces;
 public class CalificacionResource {
     private static final Logger LOOGER =Logger.getLogger(CalificacionResource.class.getName());
     
+    
+    @Inject
+    private CalificacionLogic calificacionLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
+    
+
+    
     @POST
-    public CalificacionDTO createCalificacion(CalificacionDTO calificacion)
-    {
-        return calificacion;
+    public CalificacionDTO createCalificacion(CalificacionDTO calificacion) throws BusinessLogicException {
+        
+        CalificacionDTO nuevaCalificacionDTO = new CalificacionDTO(calificacionLogic.createCalificacion(calificacion.toEntity()));
+        return nuevaCalificacionDTO;
     }
 }
