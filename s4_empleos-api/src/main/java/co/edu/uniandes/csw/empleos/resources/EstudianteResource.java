1ed21e0474b98a914b4b5b22078b1c407d18e323
Miguel Ramos <ma.ramosh@uniandes.edu.co>
2019-10-09 19:07:25 -0500

    Test de postman finalizados, implementación clase IT

--- s4_empleos-api/src/main/java/co/edu/uniandes/csw/empleos/resources/EstudianteResource.java
+++ s4_empleos-api/src/main/java/co/edu/uniandes/csw/empleos/resources/EstudianteResource.java
@@ -7,13 +7,6 @@ package co.edu.uniandes.csw.empleos.resources;
 
 
 import co.edu.uniandes.csw.empleos.dtos.EstudianteDTO;
-import co.edu.uniandes.csw.empleos.ejb.EstudianteLogic;
-import co.edu.uniandes.csw.empleos.entities.EstudianteEntity;
-import co.edu.uniandes.csw.empleos.exceptions.BusinessLogicException;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import co.edu.uniandes.csw.empleos.dtos.EstudianteDetailDTO;
 import co.edu.uniandes.csw.empleos.ejb.EstudianteCalificacionesLogic;
 import co.edu.uniandes.csw.empleos.ejb.EstudianteCuentaBancariaLogic;
@@ -45,35 +38,6 @@ import javax.ws.rs.WebApplicationException;
 @Consumes("application/json")
 @RequestScoped
 class EstudianteResource {
-    
-    
-    
-    
-     /**
-     * Busca la tarjeta con el id asociado recibido en la URL y lo devuelve.
-     *
-     * @param tarjetaId Identificador de la tarjeta que se esta buscando. Este debe
-     * ser una cadena de dígitos.
-     * @return JSON {@link TarjetaDeCreditoDTO} - El libro buscado
-     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
-     * Error de lógica que se genera cuando no se encuentra la tarjeta.
-     */
-    @GET
-    @Path("{estudiantesId: \\d+}")
-    public EstudianteDTO getTarjeta(@PathParam("estudiantesId") Long estudianteId) throws BusinessLogicException 
-    {
-        
-        EstudianteEntity entity = estudianteLogic.getEstudiante(estudianteId);
-        
-        if (entity == null) {
-            throw new WebApplicationException("El recurso /estudiantes/" + estudianteId + "no existe.", 404);
-        }
-        
-        EstudianteDTO estudianteDTO = new EstudianteDTO(entity);
-        
-        return estudianteDTO;
-    }
-    
 
     // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
     @Inject
