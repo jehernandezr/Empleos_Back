fe208b2fa01b17c42bd3317ab51e2de459e469e8
jeberdugo10 <je.berdugo10@uniandes.edu.co>
2019-10-29 13:31:13 -0500

    ofertaIT3

--- s4_empleos-api/sql/s4_empleos.sql
+++ s4_empleos-api/sql/s4_empleos.sql
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
+
 --insert into EstudianteEntity (id, nombre, idMedioDepago, carrera, correo, calificacionPromedio, horarioDeTrabajo, semestre) values(1, 'David', 1, 'Sistemas', 'algo@uniandes.edu.co', 2, 'a', 4);
-insert into ContratistaEntity (id, contrasena, email, esExterno, rutaImagen) values(2, 'Dsdf', 'dsa@hotmail.com', 'true','dsfs');
+insert into ContratistaEntity (id, contrasena, email, esExterno, rutaImagen) values(20, 'Dsdf', 'dsa@hotmail.com', 'true','dsfs');
 SELECT * FROM COntratistaEntity
-=======
+
 delete from EstudianteEntity;
 delete from TarjetaDeCreditoEntity;
 delete from CalificacionEntity;
 delete from FacturaEntity;
-<<<<<<< HEAD
+
 delete from EstudianteEntity;
-=======
+
 
 insert into EstudianteEntity (nombre, idMedioDePago, carrera, correo, calificacionPromedio, horarioDeTrabajo, semestre)
 values ('David', 0, 'Sistemas', 'awdawd@uniandes.edu.co', 2.0, 'a', 4);
