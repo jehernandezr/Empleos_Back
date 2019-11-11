464524e0f7e7f3b1f94ba28bbe8aea45321befe3
Jonatan Esmits Hernandez Rubio <je.hernandezr@uniandes.edu.co>
2019-11-11 13:49:00 -0500

    cambios de cuenta De Cobro

--- s4_empleos-api/sql/s4_empleos.sql
+++ s4_empleos-api/sql/s4_empleos.sql
@@ -2,7 +2,7 @@
 --insert into EstudianteEntity (id, nombre, idMedioDepago, carrera, correo, calificacionPromedio, horarioDeTrabajo, semestre) values(1, 'David', 1, 'Sistemas', 'algo@uniandes.edu.co', 2, 'a', 4);
 insert into ContratistaEntity (id, contrasena, email, esExterno, rutaImagen) values(20, 'Dsdf', 'dsa@hotmail.com', 'true','dsfs');
 SELECT * FROM COntratistaEntity
-
+SELECT * FROM CuentaDeCobroEntity;
 delete from EstudianteEntity;
 delete from TarjetaDeCreditoEntity;
 delete from CalificacionEntity;
@@ -10,8 +10,8 @@ delete from FacturaEntity;
 
 delete from EstudianteEntity;
 
-
+insert into CuentaDeCobroEntity (numeroCuentaDeCobro,contratista,fecha, valor,nombreEstudiante,concepto)
+values(1,null,null,23,'benito','f');
 insert into EstudianteEntity (nombre, idMedioDePago, carrera, correo, calificacionPromedio, horarioDeTrabajo, semestre)
 values ('David', 0, 'Sistemas', 'awdawd@uniandes.edu.co', 2.0, 'a', 4);
->>>>>>> 1e06ed5a16a1eafd8d6236c45eae6ac999dc9f4f
->>>>>>> 3f1dffeb1ab6931b3c5ed5082198886cab915c8a
+
