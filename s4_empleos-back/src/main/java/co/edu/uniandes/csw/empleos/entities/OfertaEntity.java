66275787c7475b37d4f905dcdcc21a767de35faa
Jonatan Esmits Hernandez Rubio <je.hernandezr@uniandes.edu.co>
2019-12-05 11:16:24 -0500

    cambios

--- s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/entities/OfertaEntity.java
+++ s4_empleos-back/src/main/java/co/edu/uniandes/csw/empleos/entities/OfertaEntity.java
@@ -2,10 +2,12 @@ package co.edu.uniandes.csw.empleos.entities;
 
 import java.io.Serializable;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.PersistenceContext;
 import uk.co.jemos.podam.common.PodamExclude;
 
 /**
@@ -20,7 +22,7 @@ public class OfertaEntity extends BaseEntity implements  Serializable{
      * Contratista que creo la oferta.
      */
     @PodamExclude
-    @ManyToOne()
+    @ManyToOne(cascade = CascadeType.PERSIST )
     private ContratistaEntity contratista;
 
     /**
@@ -110,7 +112,8 @@ public class OfertaEntity extends BaseEntity implements  Serializable{
     @PodamExclude
     @OneToOne(
             mappedBy = "oferta",
-            fetch = FetchType.LAZY
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.PERSIST
     )
     private TrabajoEntity trabajo;
 
