e0d627c002c98af9890635fc3d5aed332b83e8d7
Miguel Ramos <ma.ramosh@uniandes.edu.co>
2019-12-05 02:13:25 -0500

    Postman tarjeta de credito

--- s4_empleos-api/src/main/java/co/edu/uniandes/csw/empleos/resources/TarjetaDeCreditoResource.java
+++ s4_empleos-api/src/main/java/co/edu/uniandes/csw/empleos/resources/TarjetaDeCreditoResource.java
@@ -25,6 +25,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 
 /**
@@ -157,16 +158,16 @@ public class TarjetaDeCreditoResource {
      */
     @DELETE
     @Path("{tarjetasId: \\d+}")
-    public void deleteTarjeta(@PathParam("tarjetasId") Long tarjetaId) throws BusinessLogicException {
+    public void deleteTarjeta(@QueryParam("token") String pToken, @PathParam("tarjetasId") Long tarjetaId) throws BusinessLogicException {
 
         TarjetaDeCreditoEntity entity = tarjetaLogic.getTarjetaCredito(tarjetaId);
-        TarjetaDeCreditoDTO calDTO = new TarjetaDeCreditoDTO(entity);
+        
 
         if (entity == null) {
             throw new WebApplicationException(RECURSO + tarjetaId + NO_EXISTE, 404);
         }
 
-        String token = calDTO.getToken();
+        String token = pToken;
         TokenEntity tok = tokenLogic.getTokenByToken(token);
         if (tok == null) {
 
